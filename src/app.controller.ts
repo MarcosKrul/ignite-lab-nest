e02890cc2724e1e5cc4afa8a5f30560564c330e6
Marcos Renan Krul <marcoskrul2k@gmail.com>
2022-12-31 11:18:00 -0300

    create & read routes - initial setup

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,34 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma.service';
+import { randomUUID } from 'node:crypto';
 
-@Controller()
+@Controller('notifications')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly prismaService: PrismaService,
+  ) {}
 
-  @Get()
+  @Get('hello')
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get()
+  list() {
+    return this.prismaService.notification.findMany();
+  }
+
+  @Post()
+  async create() {
+    await this.prismaService.notification.create({
+      data: {
+        id: randomUUID(),
+        category: 'social',
+        content: 'Você tem um novo pedido de amizade!',
+        recipientId: randomUUID(),
+      },
+    });
   }
 }
